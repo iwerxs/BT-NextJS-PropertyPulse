0f7a8599435098979a040f3963c2b7f4756c1c0e
Iw Orxs <iworxs@gmail.com>
2024-08-06 15:21:08 +0100

    loading page

---
+++ app/loading.jsx
@@ -0,0 +1,23 @@
+// client side component
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const LoadingPage = () => {
+  return (
+    <>
+      <ClipLoader
+        color='#3b82f6'
+        cssOverride={override}
+        size={150}
+        aria-label='Loading Spinner'
+      />
+    </>
+  );
+};
+
+export default LoadingPage;
