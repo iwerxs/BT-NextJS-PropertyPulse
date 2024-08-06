920610813b475b751700fb41b2d767725a9903a8
Iw Orxs <iworxs@gmail.com>
2024-08-06 19:17:00 +0100

    details for individual property

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -3,6 +3,7 @@ import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -24,6 +25,7 @@ const PropertyPage = async ({ params }) => {
         <div className='container m-auto py-10 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
             {/* Property Info */}
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
