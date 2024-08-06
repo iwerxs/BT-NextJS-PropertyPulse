6cc128514902143470d98f7f0b74ed55af2b4095
Iw Orxs <iworxs@gmail.com>
2024-08-06 13:59:36 +0100

    home properties card

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -36,7 +36,7 @@ const PropertyCard = ({ property }) => {
             <h3 className='text-xl font-bold'>{property.name}</h3>
           </div>
           <h3 className='absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right'>
-            $4,200/mo
+            {getRateDisplay()}
           </h3>
 
           <div className='flex justify-center gap-4 text-gray-500 mb-4'>
